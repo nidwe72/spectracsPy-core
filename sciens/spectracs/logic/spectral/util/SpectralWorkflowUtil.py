e8cc9decae354e82c4b1cbcd1e01e5dd3539f9bb
nidwe72 <edwin.roth@gmx.at>
2023-04-10 02:32:51 +0200

    * measurement integration * SpectralWorkflow integration * SpectraContainer: added; a universal model to hold Spectrum instances; probably we will need some SpectraContainerRole enumeration to give a hint how the spectra within can be used

--- sciens/spectracs/logic/spectral/util/SpectralWorkflowUtil.py
+++ sciens/spectracs/logic/spectral/util/SpectralWorkflowUtil.py
@@ -3,6 +3,7 @@ from spectracs.model.spectral.SpectralWorkflow import SpectralWorkflow
 from spectracs.model.spectral.SpectralWorkflowPhase import SpectralWorkflowPhase
 from spectracs.model.spectral.workflow.phase.acquireView.AcquireViewSpectralWorkflowPhase import \
     AcquireViewSpectralWorkflowPhase
+from spectracs.model.spectral.workflow.phase.setp.AcquireViewSpectralWorkflowStep import AcquireViewSpectralWorkflowStep
 
 
 class SpectralWorkflowUtil(Singleton):
@@ -11,4 +12,8 @@ class SpectralWorkflowUtil(Singleton):
         result=SpectralWorkflow()
         acquireViewPhase = AcquireViewSpectralWorkflowPhase()
         result.addToPhases(acquireViewPhase)
+
+        acquireViewSpectralWorkflowStep = AcquireViewSpectralWorkflowStep()
+        acquireViewPhase.addToSteps(acquireViewSpectralWorkflowStep)
+
         return result
